43c92638b1f7844e5d60b754ac7598350f36b1a3
Mike Worden <michael.worden@icloud.com>
2022-01-11 16:59:54 -0700

    adding uitablecontroller subclass

---
+++ LootLogger/ItemsViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ItemsViewController.swift
+//  LootLogger
+//
+//  Created by Michael Worden on 1/11/22.
+//
+
+import UIKit
+
+class ItemsViewController: UITableViewController {
+    
+    
+    
+}
