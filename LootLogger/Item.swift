3555c8762f5479371ec46cc52614e0a3ace1971c
Mike Worden <michael.worden@icloud.com>
2022-01-12 15:10:55 -0700

    End of exercise

--- LootLogger/Item.swift
+++ LootLogger/Item.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class Item {
+class Item : Equatable {
     var name: String
     var valueInDollars: Int
     var serialNumber: String?
@@ -41,6 +41,13 @@ class Item {
             self.init(name: "", serialNumber: nil, valueInDollars: 0)
         }
     }
+    static func ==(lhs: Item, rhs: Item) -> Bool {
+            return lhs.name == rhs.name
+                && lhs.serialNumber == rhs.serialNumber
+                && lhs.valueInDollars == rhs.valueInDollars
+                && lhs.dateCreated == rhs.dateCreated
+        }
 
+    
 }
 
