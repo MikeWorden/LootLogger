82aeb25b4b9be4ec3a871ab71f907b69dbd1a893
Mike Worden <michael.worden@icloud.com>
2022-01-24 17:24:31 -0700

    Implemented codable protocol

--- LootLogger/ItemStore.swift
+++ LootLogger/ItemStore.swift
@@ -10,6 +10,17 @@ import UIKit
 class ItemStore  {
 
     var allItems = [Item]()
+    let itemArchiveURL: URL = {
+        let documentsDirectories =
+            FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        let documentDirectory = documentsDirectories.first!
+        return documentDirectory.appendingPathComponent("items.plist")
+    }()
+
+    
+    
+    
+    
     @discardableResult func createItem() -> Item {
         let newItem = Item(random: true)
         allItems.append(newItem)
@@ -21,6 +32,21 @@ class ItemStore  {
         }
     }*/
     
+    
+    func saveChanges() -> Bool {
+
+        do {
+            let encoder = PropertyListEncoder()
+            let data = try encoder.encode(allItems)
+        } catch let encodingError {
+            print("Error encoding allItems: \(encodingError)")
+
+        }
+        
+
+        return false
+    }
+
     func removeItem(_ item: Item) {
         if let index = allItems.firstIndex(of: item) {
             allItems.remove(at: index)
