eb6a83f229848836ea8b68299b569b5d5facd05b
Mike Worden <michael.worden@icloud.com>
2022-01-24 17:37:26 -0700

    Implemented encode function

--- LootLogger/ItemStore.swift
+++ LootLogger/ItemStore.swift
@@ -31,21 +31,31 @@ class ItemStore  {
             createItem()
         }
     }*/
-    
-    
-    func saveChanges() -> Bool {
+    init() {
+        let notificationCenter = NotificationCenter.default
+        notificationCenter.addObserver(self,
+                                       selector: #selector(saveChanges),
+                                       name: UIScene.didEnterBackgroundNotification,
+                                       object: nil)
+    }
+
+   
+    @objc func saveChanges() -> Bool  {
+        print("Saving items to: \(itemArchiveURL)")
 
         do {
             let encoder = PropertyListEncoder()
             let data = try encoder.encode(allItems)
+            try data.write(to: itemArchiveURL, options: [.atomic])
+            print("Saved all of the items")
+            return true
         } catch let encodingError {
             print("Error encoding allItems: \(encodingError)")
-
+            return false
         }
-        
 
-        return false
     }
+     
 
     func removeItem(_ item: Item) {
         if let index = allItems.firstIndex(of: item) {
