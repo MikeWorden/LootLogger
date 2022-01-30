b9828cdd09c9934672ea17a3fdabb0fc913d7a05
Mike Worden <michael.worden@icloud.com>
2022-01-30 14:49:36 -0700

    Formatting, Marking of code

--- LootLogger/DetailViewController.swift
+++ LootLogger/DetailViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class DetailViewController: UIViewController, UITextFieldDelegate, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
   
+	// Mark -- IBOutlets
     @IBOutlet var nameField: UITextField!
     @IBOutlet var serialNumberField: UITextField!
     @IBOutlet var valueField: UITextField!
@@ -17,7 +18,7 @@ class DetailViewController: UIViewController, UITextFieldDelegate, UINavigationC
 	var imageStore: ImageStore!
 
 	
-    
+    // Mark -- IBActions
 	@IBAction func deletePhoto(_ sender: Any) {
 		//remove the image from the imageView
 		imageView.image = nil
@@ -30,20 +31,107 @@ class DetailViewController: UIViewController, UITextFieldDelegate, UINavigationC
         view.endEditing(true)
     }
     
-    func imagePicker(for sourceType: UIImagePickerController.SourceType)
-                                                            -> UIImagePickerController {
-        let imagePicker = UIImagePickerController()
-																
-        imagePicker.sourceType = sourceType
-        imagePicker.delegate = self
-		imagePicker.allowsEditing = true
-                                                                
-                                                                
-        return imagePicker
-    }
+	
+	//Mark -- Functions
+	@IBAction func choosePhotoSource(_ sender: UIBarButtonItem) {
 
+		let alertController = UIAlertController(title: nil,
+											message: nil,   preferredStyle: .actionSheet)
+
+
+		alertController.modalPresentationStyle = .popover
+		alertController.popoverPresentationController?.barButtonItem = sender
+	
+		if UIImagePickerController.isSourceTypeAvailable(.camera) {
+			let cameraAction = UIAlertAction(title: "Camera", style: .default) { _ in
+			// print("Present camera")
+			let imagePicker = self.imagePicker(for: .camera)
+			imagePicker.allowsEditing = true
+			self.present(imagePicker, animated: true, completion: nil)
+			
+		}
+		alertController.addAction(cameraAction)
+	}
+
+	
+
+		let photoLibraryAction
+			= UIAlertAction(title: "Photo Library", style: .default) { _ in
+		//print("Present photo library")
+				let imagePicker = self.imagePicker(for: .photoLibrary)
+				self.present(imagePicker, animated: true, completion: nil)
+			}
+		alertController.addAction(photoLibraryAction)
+	
+	
+		let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+		alertController.addAction(cancelAction)
+		present(alertController, animated: true, completion: nil)
+
+
+}
+
+
+
+
+	var item: Item! {
+		didSet {
+			//navigationItem.title = item.name
+			
+		}
+	}
+	
+	let numberFormatter: NumberFormatter = {
+		let formatter = NumberFormatter()
+		formatter.numberStyle = .decimal
+		formatter.minimumFractionDigits = 2
+		formatter.maximumFractionDigits = 2
+		return formatter
+	}()
+	
+	let dateFormatter: DateFormatter = {
+		let formatter = DateFormatter()
+		formatter.dateStyle = .medium
+		formatter.timeStyle = .none
+		return formatter
+	}()
+
+
+
+	override func viewWillAppear(_ animated: Bool) {
+		super.viewWillAppear(animated)
+		
+		nameField.text = item.name
+		serialNumberField.text = item.serialNumber
+		//valueField.text = "\(item.valueInDollars)"
+		//dateLabel.text = "\(item.dateCreated)"
+		valueField.text =
+		numberFormatter.string(from: NSNumber(value:item.valueInDollars))
+		dateLabel.text = dateFormatter.string(from: item.dateCreated)
+		// Get the item key
+		let key = item.itemKey
+		
+		// if there is an associated image, retrieve & display it
+		let imageToDisplay = imageStore.image(forKey: key)
+		imageView.image = imageToDisplay
+		
+		
+		
+	}
+	func imagePicker(for sourceType: UIImagePickerController.SourceType)
+	-> UIImagePickerController {
+		let imagePicker = UIImagePickerController()
+		
+		imagePicker.sourceType = sourceType
+		imagePicker.delegate = self
+		imagePicker.allowsEditing = true
+		
+		
+		return imagePicker
+	}
+	
 	func imagePickerController(_ picker: UIImagePickerController,
-			didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {
+							   didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {
 		
 		
 		
@@ -54,124 +142,39 @@ class DetailViewController: UIViewController, UITextFieldDelegate, UINavigationC
 		
 		// Store the image in the ImageStore for the item's key
 		imageStore.setImage(image, forKey: item.itemKey)
-
-
+		
+		
 		// Put that image on the screen in the image view
 		imageView.image = image
-
+		
 		// Take image picker off the screen - you must call this dismiss method
 		dismiss(animated: true, completion: nil)
-
-	}
-			
-    @IBAction func choosePhotoSource(_ sender: UIBarButtonItem) {
-        
-        let alertController = UIAlertController(title: nil,
-                                                    message: nil,   preferredStyle: .actionSheet)
-        
-        
-        alertController.modalPresentationStyle = .popover
-            alertController.popoverPresentationController?.barButtonItem = sender
-
-        if UIImagePickerController.isSourceTypeAvailable(.camera) {
-            let cameraAction = UIAlertAction(title: "Camera", style: .default) { _ in
-                   // print("Present camera")
-                let imagePicker = self.imagePicker(for: .camera)
-				imagePicker.allowsEditing = true
-				self.present(imagePicker, animated: true, completion: nil)
-				                
-            }
-            alertController.addAction(cameraAction)
-        }
-        
-            
-
-        let photoLibraryAction
-                    = UIAlertAction(title: "Photo Library", style: .default) { _ in
-                //print("Present photo library")
-			let imagePicker = self.imagePicker(for: .photoLibrary)
-			self.present(imagePicker, animated: true, completion: nil)
-        }
-		alertController.addAction(photoLibraryAction)
-
 		
-        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
-            alertController.addAction(cancelAction)
-        present(alertController, animated: true, completion: nil)
-        
-        
-    }
-    
-
-    
-    
-    var item: Item! {
-        didSet {
-            //navigationItem.title = item.name
-           
-        }
-    }
-
-    let numberFormatter: NumberFormatter = {
-        let formatter = NumberFormatter()
-        formatter.numberStyle = .decimal
-        formatter.minimumFractionDigits = 2
-        formatter.maximumFractionDigits = 2
-        return formatter
-    }()
-
-    let dateFormatter: DateFormatter = {
-        let formatter = DateFormatter()
-        formatter.dateStyle = .medium
-        formatter.timeStyle = .none
-        return formatter
-    }()
-
-    
-
-	override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-
-        nameField.text = item.name
-        serialNumberField.text = item.serialNumber
-        //valueField.text = "\(item.valueInDollars)"
-        //dateLabel.text = "\(item.dateCreated)"
-        valueField.text =
-                numberFormatter.string(from: NSNumber(value:item.valueInDollars))
-        dateLabel.text = dateFormatter.string(from: item.dateCreated)
-		// Get the item key
-		let key = item.itemKey
-		
-		// if there is an associated image, retrieve & display it
-		let imageToDisplay = imageStore.image(forKey: key)
-		imageView.image = imageToDisplay
-
-            
-            
 	}
-    
+	
+	
 	
 	
 	
 	override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
-
-        // "Save" changes to item
-        item.name = nameField.text ?? ""
-        item.serialNumber = serialNumberField.text
-        
-        // Clear first responder
-            view.endEditing(true)
-
-
-        if let valueText = valueField.text,
-            let value = numberFormatter.number(from: valueText) {
-            item.valueInDollars = value.intValue
-        } else {
-            item.valueInDollars = 0
-        }
-    }
-    
+		super.viewWillDisappear(animated)
+		
+		// "Save" changes to item
+		item.name = nameField.text ?? ""
+		item.serialNumber = serialNumberField.text
+		
+		// Clear first responder
+		view.endEditing(true)
+		
+		
+		if let valueText = valueField.text,
+		   let value = numberFormatter.number(from: valueText) {
+			item.valueInDollars = value.intValue
+		} else {
+			item.valueInDollars = 0
+		}
+	}
+	
     
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         textField.resignFirstResponder()
